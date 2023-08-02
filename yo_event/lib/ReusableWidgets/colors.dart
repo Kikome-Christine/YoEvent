d027013c5ec7659f4a57b1b08ef2acf7f5a1b6b3
Geoffrey256 <okumuojgeoffrey@gmail.com>
2023-08-02 23:58:38 +0300

    Modified colors.dart

--- yo_event/lib/ReusableWidgets/colors.dart
+++ yo_event/lib/ReusableWidgets/colors.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-hexStringToColors(String hexColor) {
-  hexColor = hexColor.toUpperCase().replaceAll("#", "");
-  if (hexColor.length == 6) {
-    hexColor = "FF" + hexColor;
-  }
-  return Color(int.parse(hexColor, radix: 16));
+class AppColors {
+  static final Color AppBarColor = const Color(0x1E6091);
+  static final Color ButtonColor = const Color(0x1E6091);
+  static final Color MainColor = const Color(0x8D99AE);
+  static final Color IconColor = const Color(0x1E6091);
+  // static final Color AppBar = const Color(0x1E6091);
+  // static final Color AppBar = const Color(0x1E6091);
 }
