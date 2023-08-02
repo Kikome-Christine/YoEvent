0f1c11755d0afabfb0e8ccaaab4e13ab62f0a551
Geoffrey256 <okumuojgeoffrey@gmail.com>
2023-08-02 11:08:04 +0300

    Modified main.dart

--- yo_event/lib/main.dart
+++ yo_event/lib/main.dart
@@ -1,6 +1,5 @@
- 
 import 'package:flutter/material.dart';
- 
+
 import 'package:yo_event/views/splash_screen.dart';
 
 void main() {
@@ -20,67 +19,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-
-// void main(){
-//   runApp(Home());
-// }
-
-// class Home extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       color: Colors.blueGrey,
-//       home: Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.amber,
-//         title: Text("Event Hub",
-//         style: TextStyle(
-//           color: Colors.black,
-//           fontWeight: FontWeight.bold,
-//           fontStyle: FontStyle.italic,
-//         ),
-//         ),
-//       ),
-//      //body: Home(),
-//     ),
-//     );
-//   }
-// }
-
-
-
-// class Home extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.grey,
-//           title: Text("EVENT HUB",
-//           // style: TextStyle(
-//           //   color: Colors.black,
-//           // ), 
-//           ),
-//         ),
-//         //body: SignInPage(),
-//       ),
-//     );
-//   }
-// }
